8cbec37947c393e5aa2abe19070be28f2ba932c5
Jin Choi <jinchoi@10-19-250-83.dynapool.wireless.nyu.edu>
2024-04-19 15:35:42 -0400

    complete interaction plot

--- .ipynb_checkpoints/dashboard-checkpoint.py
+++ .ipynb_checkpoints/dashboard-checkpoint.py
@@ -13,25 +13,32 @@ def main():
     
     def load_data(loc):
         path = f"{loc.lower()}_cooccurrence.csv"
-        return pd.read_csv(f"/data/{path}", index_col=0)
+        return pd.read_csv(f"./data/{path}", index_col=0)
     
     # Function to get top N co-occurring pairs
     def top_n_cooccur_pairs(df, top_n):
-        pairs = df.stack()
-        pairs = pairs[pairs.index.get_level_values(0) < pairs.index.get_level_values(1)]
-        top_pairs = pairs.sort_values(ascending=False).head(top_n)
+    
+        pairs = df.stack().reset_index()
+        pairs.columns = ['Term1', 'Term2', 'Co-occurrence']
+        pairs = pairs[pairs['Term1'] < pairs['Term2']]
+        top_pairs = pairs.sort_values(by='Co-occurrence', ascending=False).head(top_n)
+        
         return top_pairs
     
     # Displaying the top N co-occurring pairs
     if st.button('Show Top Pairs'):
         df = load_data(location) 
         top_pairs = top_n_cooccur_pairs(df, top_n)
-        top_pairs.index = [" & ".join(map(str, idx)) for idx in top_pairs.index]
-    
-        fig = px.bar(top_pairs, x=top_pairs.values, y=top_pairs.index, orientation='h',
-                     labels={'y': 'Pairs', 'x': 'Counts'},
+        top_pairs['Pair'] = top_pairs['Term1'] + " & " + top_pairs['Term2']
+        
+        # Calculate figure height: base height + (number of bars * height per bar)
+        figure_height = max(400, 20 * len(top_pairs))
+        fig = px.bar(top_pairs, x='Co-occurrence', y='Pair', orientation='h',
+                     labels={'Pair': 'Pairs', 'Co-occurrence': 'Counts'},
                      title=f"Top {top_n} Co-occurring Pairs in {location}")
         
+        fig.update_yaxes(autorange="reversed")
+        fig.update_layout(height=figure_height)
         st.plotly_chart(fig)
 
 if __name__ == "__main__":
